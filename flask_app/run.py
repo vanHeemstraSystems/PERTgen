a58f424dd254dd91f535cccf84fa4a6f27c81a7e
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-10-13 21:41:54 +0200

    Update run.py

--- flask_app/run.py
+++ flask_app/run.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+import os
+from app import create_app
+from app.extensions import db, socketio
+from app.seeds.seed_schedules import seed_schedules
 
+app = create_app()
+
+if __name__ == "__main__":
+    socketio.run(app)
+    with app.app_context():
+        seed_schedules()
+      
